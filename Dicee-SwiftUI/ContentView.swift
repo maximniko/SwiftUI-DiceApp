327224b3312cfde94fffc007c5ce79ca81b35b4f
maximniko <immaximniko@gmail.com>
2020-03-03 22:19:20 +0200

    Initial Commit

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Maxim Niko on 03.03.2020.
+//  Copyright © 2020 Maxim Niko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
